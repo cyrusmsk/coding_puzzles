078f8215244f561e8817a599bc3fcb9f3a98b460
Sergei Giniatulin <kornburn@gmail.com>
2025-03-08 00:45:27 +0200

    Added Day1 for i18n

---
+++ i18n_puzzles/d1/app.d
@@ -0,0 +1,27 @@
+import std.stdio;
+import std.range;
+
+void main() {
+    File file = File("input.txt", "r");
+    bool sms, twit;
+
+    ulong charge;
+
+    foreach(line; file.byLine) {
+        sms = true;
+        twit = true;
+
+        if (line.length > 160)
+            sms = false;
+        if (line.walkLength > 140)
+            twit = false;
+        
+        if (sms && twit)
+            charge += 13;
+        else if (sms)
+            charge += 11;
+        else if (twit)
+            charge += 7;
+    }
+    writeln(charge);
+}
---
+++ i18n_puzzles/readme.md
@@ -0,0 +1,4 @@
+# Internationalization Puzzles
+
+New interesting puzzle - https://i18n-puzzles.com
+Similar to AoC
